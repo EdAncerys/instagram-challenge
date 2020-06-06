a9da6408d037add844ebf3afc8229a75ce031568
Ed <lookatemail@gmail.com>
2020-06-06 12:19:35 +0100

    user validation if email is not valid

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,8 +16,13 @@ RSpec.describe User, type: :model do
     expect(user).to_not be_valid
   end
 
-  it "User is not valid when eemail is nil" do
+  it "User is not valid when email is nil" do
     user = User.create(fname: 'John', lname: 'Doe', email: nil, password: 'password')
+    expect(user).to_not be_valid
+  end
+
+  it "User is not valid when email is not valid" do
+    user = User.create(fname: 'John', lname: 'Doe', email: 'notValid', password: 'password')
     expect(user).to_not be_valid
   end
 end
